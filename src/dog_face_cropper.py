c6d3816a7033bda99933151bc7a621a6b34a0a9e
Luciano Chehem <luciano.chehem@gmail.com>
2021-04-20 21:07:11 -0300

    Main files

---
+++ src/dog_face_cropper.py
@@ -0,0 +1,61 @@
+import dlib
+import cv2
+import os
+import imutils
+
+from utils import download_image, show_with_points
+from rotate import DogFaceRotate
+from crop import DogFaceCrop
+from resize import DogFaceResize
+
+class DogFaceCropper():
+
+  def __init__(self, detectors_path):
+  
+    detector = dlib.cnn_face_detection_model_v1(os.path.join(detectors_path, 'dogHeadDetector.dat'))
+    predictor = dlib.shape_predictor(os.path.join(detectors_path, 'landmarkDetector.dat'))
+    
+    self.rotator = DogFaceRotate(detector, predictor)
+    self.cropper = DogFaceCrop(detector, predictor)
+    self.resizer = DogFaceResize()
+
+  def process_image(self, img):
+  
+    tracking = {}
+  
+    show_with_points(img, [], figsize=(4, 4))
+    #img_result = img.copy()
+
+    img_resized = self.resizer.input_resize(img, max_size = 300)
+    tracking['ratio'] = img_resized.shape[0]/img.shape[0] # using height
+
+    img_rotated, tracking['angle'] = self.rotator.rotate(img_resized)
+    #show_with_points(img_rotated, [])
+
+    img_cropped, tracking['cropping_points'] = self.cropper.crop(img_rotated)
+    #show_with_points(img_cropped, [])
+    
+    img_reprocessed = self.reprocess(img, tracking)
+
+    img_resized = self.resizer.final_resize(img_reprocessed)
+    show_with_points(img_resized, [], figsize=(4, 4))
+    
+    return img_resized
+    
+  def reprocess(self, img, tracking):
+    img = imutils.rotate_bound(img, tracking['angle'])
+    adjused_cropping_points = tuple([round(p/tracking['ratio']) for p in tracking['cropping_points']])
+    return self.cropper.crop_with_padding(img, *adjused_cropping_points)
+
+  def process_file(self, img_url, save_as_path = None):
+  
+    img_path = download_image(img_url)
+
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    
+    img_processed = self.process_image(img)
+
+    if save_as_path is not None:
+      img_out = cv2.cvtColor(img_processed, cv2.COLOR_RGB2BGR)
+      cv2.imwrite(save_as_path, img_out)
---
+++ src/utils.py
@@ -0,0 +1,31 @@
+import urllib
+import imghdr
+import time
+import shutil
+import cv2
+import matplotlib.pyplot as plt
+from imutils import face_utils
+
+def download_image(img_url):
+  tmp_path, _ = urllib.request.urlretrieve(img_url)
+  ext = imghdr.what(tmp_path)
+  to = str(time.time()).replace('.', 's') + '.' + ('unknown' if ext is None else ext)
+  shutil.copyfile(tmp_path, to)
+  return to
+
+def show_with_points(img, points, figsize=(16, 16)):
+  img_result = img.copy()
+  for i, p in enumerate(points):
+    p = (round(p[0]), round(p[1]))
+    cv2.circle(img_result, center=p, radius=3, color=(0,0,255), thickness=-1, lineType=cv2.LINE_AA)
+    cv2.putText(img_result, str(i), p, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+  plt.figure(figsize=figsize)
+  plt.imshow(img_result)
+
+def get_detection_and_shape(detector, predictor, img):
+  dets = detector(img, upsample_num_times=1)
+  assert len(dets) == 1, "Ver qué hago en estos casos... (quedarme con la mas grande? la que tiene mayor confidence?)"
+  d = dets[0]
+  shape = predictor(img, d.rect)
+  shape = face_utils.shape_to_np(shape)
+  return d, [tuple(x) for x in shape]
