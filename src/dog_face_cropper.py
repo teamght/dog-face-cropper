f10bb6265779f5d7d6ca6b8e76aefb0240e7255a
Francisco Cerna Fukuzaki <fcernaf@gmail.com>
2021-05-10 03:44:36 -0500

    Retorno de variable Flag

--- src/dog_face_cropper.py
+++ src/dog_face_cropper.py
@@ -2,8 +2,8 @@ import dlib
 import cv2
 import os
 import imutils
+from datetime import datetime
 
-from .utils import download_image, show_with_points
 from .rotate import DogFaceRotate
 from .crop import DogFaceCrop
 from .resize import DogFaceResize
@@ -20,27 +20,22 @@ class DogFaceCropper():
     self.resizer = DogFaceResize()
 
   def process_image(self, img):
-  
-    tracking = {}
-  
-    show_with_points(img, [], figsize=(4, 4))
-    #img_result = img.copy()
-
-    img_resized = self.resizer.input_resize(img, max_size = 300)
-    tracking['ratio'] = img_resized.shape[0]/img.shape[0] # using height
-
-    img_rotated, tracking['angle'] = self.rotator.rotate(img_resized)
-    #show_with_points(img_rotated, [])
-
-    img_cropped, tracking['cropping_points'] = self.cropper.crop(img_rotated)
-    #show_with_points(img_cropped, [])
-    
-    img_reprocessed = self.reprocess(img, tracking)
-
-    img_resized = self.resizer.final_resize(img_reprocessed)
-    show_with_points(img_resized, [], figsize=(4, 4))
-    
-    return img_resized
+    print('Iniciando procesamiento de imagen para ser recortada ... ({})'.format(datetime.now()))
+    try:
+      tracking = {}
+          
+      img_resized = self.resizer.input_resize(img, max_size = 300)
+      tracking['ratio'] = img_resized.shape[0]/img.shape[0] # using height
+      img_rotated, tracking['angle'] = self.rotator.rotate(img_resized)
+      img_cropped, tracking['cropping_points'] = self.cropper.crop(img_rotated)
+      img_reprocessed = self.reprocess(img, tracking)
+      img_resized = self.resizer.final_resize(img_reprocessed)
+      print('Fin de procesamiento de imagen para ser recortada ({})'.format(datetime.now()))
+      return True, img_resized
+    except Exception as e:
+      print('Hubo un error al procesar la imagen ({})'.format(datetime.now()))
+      print('Hubo un error. {}'.format(e))
+      return False, None
     
   def reprocess(self, img, tracking):
     img = imutils.rotate_bound(img, tracking['angle'])
@@ -52,7 +47,7 @@ class DogFaceCropper():
       img = cv2.imread(img_path)
       img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
       
-      img_processed = self.process_image(img)
+      flag, img_processed = self.process_image(img)
 
       if save_as_path is not None:
         img_out = cv2.cvtColor(img_processed, cv2.COLOR_RGB2BGR)
